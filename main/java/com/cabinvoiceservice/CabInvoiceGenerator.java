8f0497dacba027d1b5e20d60c88b8a34944b910c
shaheen5 <miyashaheen1995@gmail.com>
2021-04-30 10:13:35 +0530

    Added : Code To Display Welcome Message

---
+++ main/java/com/cabinvoiceservice/CabInvoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.cabinvoiceservice;
+
+public class CabInvoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Cab Invoice Generator Program");
+    }
+}
