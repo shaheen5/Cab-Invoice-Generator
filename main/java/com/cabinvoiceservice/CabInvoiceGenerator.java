fe756cb41b5cbc774f70de462cabce54e28f1365
shaheen5 <miyashaheen1995@gmail.com>
2021-05-01 09:49:21 +0530

    Added : Code To Calculate Fare & Test Results

--- main/java/com/cabinvoiceservice/CabInvoiceGenerator.java
+++ main/java/com/cabinvoiceservice/CabInvoiceGenerator.java
@@ -1,7 +1,12 @@
 package com.cabinvoiceservice;
 
 public class CabInvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Cab Invoice Generator Program");
+    //constants
+    private static final int COST_PER_MIN_TIME = 1;
+    private static final double  MINIMUM_COST_PER_KILOMETER = 10;
+
+    //method to calculate and return total fare
+    public double calculateFare(double distance, int time) {
+        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MIN_TIME;
     }
 }
---
+++ test/java/com/cabinvoiceservice/InvoiceServiceTest.java
@@ -0,0 +1,15 @@
+package com.cabinvoiceservice;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare(){
+        CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(25,fare,0.0);
+    }
+}
