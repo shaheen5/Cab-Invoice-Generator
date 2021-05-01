e401620db643bcdd6f91f406316c09581eedd120
shaheen5 <miyashaheen1995@gmail.com>
2021-05-01 10:09:05 +0530

    Refactor : Code To Get Minimum Fare & Test Results

--- test/java/com/cabinvoiceservice/InvoiceServiceTest.java
+++ test/java/com/cabinvoiceservice/InvoiceServiceTest.java
@@ -12,4 +12,13 @@ public class InvoiceServiceTest {
         double fare = invoiceGenerator.calculateFare(distance,time);
         Assert.assertEquals(25,fare,0.0);
     }
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinimumFare(){
+        CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(5,fare,0.0);
+    }
+
 }
